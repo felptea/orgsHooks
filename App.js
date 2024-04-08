227da41e7710060c37216d2077d261a8f5ea94be
unknown <felipe.silva@smartea.com.br>
2024-04-08 16:40:20 -0300

    orgsHooks

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView,Text} from 'react-native';
+import Home from './src/telas/Home';
+
+function App() {
+  return 
+    <Home />
+    
+}
+
+export default App;
